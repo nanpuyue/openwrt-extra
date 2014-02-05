2aac67dcfc58167c30657ea4916dd7d41a2aa8bd
南浦月 <nanpuyue@gmail.com>
2014-02-06 00:59:28 +0800

    Update aria2.lua

--- luci-extra/applications/luci-aria2/luasrc/controller/aria2.lua
+++ luci-extra/applications/luci-aria2/luasrc/controller/aria2.lua
@@ -17,7 +17,7 @@ function index()
 		return
 	end
 
-	local page = entry({"admin", "services", "aria2"}, cbi("aria2"), _("Aria2 配置"))
+	local page = entry({"admin", "services", "aria2"}, cbi("aria2"), _("Aria2 Settings"))
 	page.dependent = true
 
 end
